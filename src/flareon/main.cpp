fe67c17315ea6a9ffdd71ef2eb70a2d176d235fc
Ben Allen <benos.9k@gmail.com>
2014-07-29 00:10:31 +1200

    its a red screen!

--- src/flareon/main.cpp
+++ src/flareon/main.cpp
@@ -1,18 +1,128 @@
 
 
-
+#include <cstdio>
 #include <iostream>
+#include <chrono>
 
 #include "Initial3D.hpp"
 #include "Flareon.hpp"
 #include "Log.hpp"
 #include "Window.hpp"
+#include "Shader.hpp"
 
 using namespace std;
 using namespace ambition;
 using namespace initial3d;
 
-void display() {
+ShaderManager *shaderman;
+
+
+
+template <unsigned Width, unsigned Height>
+void draw_fullscreen_grid_adjacency() {
+	static_assert(Width >= 1 && Height >= 1, "grid must be at least 1x1");
+	static GLuint vao = 0;
+	if (vao == 0) {
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		GLuint vbo, ibo;
+		glGenBuffers(1, &vbo);
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo); // this doesnt stick to the vao
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); // this does
+		// generate x and y components
+		vector<float> x_vals, y_vals;
+		double dx = 2.0 / Width, dy = 2.0 / Height;
+		// bottom and left off-screen vertices
+		x_vals.push_back(-1.0 - dx);
+		y_vals.push_back(-1.0 - dy);
+		// on-screen vertices
+		for (unsigned i = 0; i < Width; i++) {
+			x_vals.push_back(-1.0 + dx * i);
+		}
+		for (unsigned i = 0; i < Height; i++) {
+			y_vals.push_back(-1.0 + dy * i);
+		}
+		// force the last on-screen values of x/y to be correct
+		x_vals.push_back(1.0);
+		y_vals.push_back(1.0);
+		// top and right off-screen vertices
+		x_vals.push_back(1.0 + dx);
+		y_vals.push_back(1.0 + dy);
+		// generate actual points, index 0 is reserved for 'not a vertex'
+		vector<float> points { 0.0f, 0.0f };
+		for (unsigned i = 0; i < x_vals.size(); i++) {
+			for (unsigned j = 0; j < y_vals.size(); j++) {
+				points.push_back(x_vals[i]);
+				points.push_back(y_vals[j]);
+			}
+		}
+		// generate triangle vertex indices
+		vector<GLuint> indices;
+		auto push_index = [&](int i, int j) {
+			// index 0 is reserved for 'not a vertex'
+			int ii = 0;
+			if (i >= 0 && i <= Width + 1 && j >= 0 && j <= Height + 1) {
+				ii = 1 + y_vals.size() * i + j;
+			}
+			indices.push_back(unsigned(ii));
+		};
+		for (unsigned i = 1; i < Width + 1; i++) {
+			for (unsigned j = 1; j < Height + 1; j++) {
+				// first tri (:), p1=(i,j)
+				// 6---5---4 //
+				//   \ |:\ | //
+				//     1---3 //
+				//       \ | //
+				//         2 //
+				push_index(i, j);
+				push_index((i + 1), j - 1);
+				push_index((i + 1), j);
+				push_index((i + 1), j + 1);
+				push_index(i, j + 1);
+				push_index((i - 1), j + 1);
+				// second tri (:), p6=(i,j)
+				// 4         //
+				// | \       //
+				// 5---3     //
+				// | \:| \   //
+				// 6---1---2 //
+				push_index((i + 1), j);
+				push_index((i + 2), j);
+				push_index((i + 1), j + 1);
+				push_index(i, j + 2);
+				push_index(i, j + 1);
+				push_index(i, j);
+			}
+		}
+		// upload data
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(float), &points[0], GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+		glBindBuffer(GL_ARRAY_BUFFER, 0); // cleanup
+	}
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES_ADJACENCY, Width * Height * 12, GL_UNSIGNED_INT, nullptr);
+	glBindVertexArray(0);
+}
+
+void display(const size2i &sz) {
+
+	glViewport(0, 0, sz.w, sz.h);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shaderman->getProgram("flare.glsl"));
+
+	checkGL();
+
+	draw_fullscreen_grid_adjacency<32, 32>();
+
+	checkGL();
+
+	glUseProgram(0);
+
+	glFinish();
 
 }
 
@@ -21,18 +131,33 @@ int main(int argc, char *argv[]) {
 	Window *win = createWindow().size(512, 512).title("I Choose You, Flareon!").visible(true);
 	win->makeContextCurrent();
 
+	shaderman = new ShaderManager("./res/shader");
+
 	win->onResize.attach([](const window_size_event &e) {
 
 		return false;
 	});
 
+	auto time_fps = chrono::steady_clock::now();
+	unsigned fps = 0;
+
 	while (!win->shouldClose()) {
+		auto now = chrono::steady_clock::now();
+		if (now - time_fps > chrono::seconds(1)) {
+			time_fps = now;
+			char cbuf[100];
+			sprintf(cbuf, "Flareon [%d FPS @%dx%d", fps, win->width(), win->height());
+			win->title(cbuf);
+			fps = 0;
+		}
+
 		glfwPollEvents();
 
-		display();
+		display(win->size());
 
 		glFinish();
 		win->swapBuffers();
+		fps++;
 	}
 
 	delete win;
