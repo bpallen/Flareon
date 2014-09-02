3d151b9b90e6d2f27728782a2f7fb212548011fd
Ben Allen <benos.9k@gmail.com>
2014-09-02 18:55:29 +1200

    fix up GFFT a bit

--- ext/gfft/src/gfft.cpp
+++ ext/gfft/src/gfft.cpp
@@ -1,6 +1,8 @@
 
 #include <GFFT/GFFT.h>
 
+using namespace gfft;
+using namespace std;
 
 typedef double Tp;
 
@@ -8,39 +10,39 @@ typedef double Tp;
 static int test()
 {
 	// range of the needed GFFT classes
-    const unsigned Min = 1;
-    const unsigned Max = 27;
+	const unsigned Min = 1;
+	const unsigned Max = 27;
 
 	// initialization of the object factory
-    Loki::Factory<AbstractFFT<Tp>,unsigned int> gfft_factory;
-    FactoryInit<GFFTList<GFFT,Min,Max>::Result>::apply(gfft_factory);
+	Loki::Factory<AbstractFFT<Tp>,unsigned int> gfft_factory;
+	FactoryInit<GFFTList<GFFT,Min,Max>::Result>::apply(gfft_factory);
 
-    unsigned long i;
+	unsigned long i;
 
 	// runtime definition of the data length
-    int p = 2;
+	int p = 2;
 
 	// create an instance of the GFFT
-    AbstractFFT<Tp>* gfft = gfft_factory.CreateObject(p);
+	AbstractFFT<Tp>* gfft = gfft_factory.CreateObject(p);
 
 	// sample data
-    unsigned long n = 1<<p;
-    Tp* data = new Tp [2*n];
-    for (i=0; i<n; ++i) {
-        data[2*i] = 2*i;
-        data[2*i+1] = 2*i+1;
-    }
+	unsigned long n = 1<<p;
+	Tp* data = new Tp [2*n];
+	for (i=0; i<n; ++i) {
+		data[2*i] = 2*i;
+		data[2*i+1] = 2*i+1;
+	}
 
 	// compute FFT
-    gfft->fft(data);
+	gfft->fft(data);
 
 	// print the results
-    cout<<"--------------------------------" << endl;
-      for (i=0; i<n; ++i)
-        cout << setw(10) << setprecision(5) << data[2*i] << "\t"
-             << data[2*i+1] << "I" << endl;
+	cout<<"--------------------------------" << endl;
+	  for (i=0; i<n; ++i)
+		cout << setw(10) << setprecision(5) << data[2*i] << "\t"
+			 << data[2*i+1] << "I" << endl;
 
-    delete [] data;
+	delete [] data;
 
-    return 0;
+	return 0;
 }
